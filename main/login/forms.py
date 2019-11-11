d4306723d60c98eece90c608b63646b0c936b8f4
Satyam Faujdar <fouzdarsatyam4@gmail.com>
2019-11-11 11:50:23 +0530

    2 login system for teacher and student

--- main/login/forms.py
+++ main/login/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from .models import User 
 
 class RegistrationForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
+        fields = ('username', 'first_name', 'last_name','user_type', 'email', 'password1', 'password2', )
--- main/login/models.py
+++ main/login/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+
+    USER_TYPE_CHOICES = (
+        (1, 'student'),
+        (2, 'teacher'),
+        )
+    user_type = models.PositiveSmallIntegerField(choices=USER_TYPE_CHOICES)
